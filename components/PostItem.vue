916c4054eeaf7274c77efe81465d601dc58ce9d0
Mandy <mandypletinckx13@gmail.com>
2021-03-08 19:10:52 +0100

    install and index post skeleton

---
+++ components/PostItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card mb-3">
+    <h5 class="card-header">{{ title }}</h5>
+    <div class="card-body">
+      <h5 class="card-title">{{ subtitle }}</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">See More</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: false,
+    },
+
+
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <navbar />
+    <div class="container justify-content align-items">
+      <div class="title mt-5">
+        <h2>Newest Post</h2>
+      </div>
+      <div class="post-container d-flex flex-column mt-4">
+        <post-item
+          v-for="post in posts"
+          :key="post.id"
+          :title="post.title"
+          :subtitle="post.subtitle" />
+
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Navbar from '~/components/navbar'
+import PostItem from "~/components/PostItem";
+
+export default {
+  component: {
+    Navbar,
+    PostItem
+  },
+  data (){
+    return {
+      title: 'My title from Page',
+      posts: [
+        {
+          id: '1',
+          title: 'My first post',
+          subtitle: 'My first subtitle',
+        },
+        {
+          id: '2',
+          title: 'My second post',
+          subtitle: 'My second subtitle',
+        },
+        {
+          id: '3',
+          title: 'My third post',
+          subtitle: 'My third subtitle',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
